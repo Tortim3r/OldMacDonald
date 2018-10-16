1f070204e5557351a47588fab2e0cfdb659526d1
malee33 <malee33@s.sfusd.edu>
2018-10-16 10:26:14 -0700

    started assignment

--- Chick.java
+++ Chick.java
@@ -1,4 +1,30 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+	private String myType;
+	private String myType2;
+	private String mySound;
+
+	public Chick(String type, String sound)
+	{
+		myType = type;
+		mySound = sound;
+	}
+	public Chick(String type, String type2, String sound)
+	{
+		myType = type;
+		mySound = sound;
+	}
+	public Chick()
+	{
+		myType = "unknown";
+		mySound = "unknown";
+	}
+	public String getSound() 
+	{
+		return mySound;
+	}
+	public String getType()
+	{
+		return myType;
+	}
 }
